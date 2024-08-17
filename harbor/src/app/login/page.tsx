c41c55e99359bea7fe606be2bbed915128c6dcfa
henriquejm98 <henrique.mosca@sptech.school>
2024-08-17 18:41:07 -0300

    add login migration

---
+++ harbor/src/app/login/page.tsx
@@ -0,0 +1,42 @@
+import Link from 'next/link'
+import { Container } from '@/components/Container/Container'
+import { Heading } from '@/components/Heading/Heading'
+import { Typography } from '@/components/Typography/Typography'
+import { LoginForm } from './components/LoginForm'
+import Image from 'next/image'
+
+
+export default function Login() {
+  return (
+    <div className='w-screen min-h-screen bg-gradient-to-b from-transparent to-white flex items-center justify-center relative'>
+      <Image 
+        src="/images/graphs/grafismos.svg"
+        alt="Grafismos"
+        className='absolute top-0 z-0'
+        height={ 500 }
+        width={ 550 }
+      />
+
+      <Container maxWidth="md" className="flex flex-col gap-4 z-10">
+        <div className='flex flex-col gap-2'>
+          <Heading
+            color='black'
+            size={3}
+          >
+            Comece a faturar com a solução Harbor
+          </Heading>
+
+          <Typography
+            color="body"
+            textPosition="left"
+            className="whitespace-nowrap"
+          >
+            Não possui conta? <Link href={'/cadastrar'}><span className='text-blueEnd'> Comece por aqui</span></Link>
+          </Typography>
+        </div>
+        <LoginForm />
+        
+      </Container>
+    </div>
+  )
+}
