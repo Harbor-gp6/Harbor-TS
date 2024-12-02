3a28f6d602ccc6c75f0ef641768ebbb7517d18ef
Leozinho11 <leoferraribarreto@hotmail.com>
2024-12-02 02:37:25 -0300

    Fix dates

--- src/lib/get-monthly-gross-revenue.ts
+++ src/lib/get-monthly-gross-revenue.ts
@@ -4,10 +4,17 @@ export async function GetMonthlyGrossRevenue(token: string) {
 
   const ano = new Date().getFullYear()
   const mes = String(new Date().getMonth() + 1).padStart(2, '0')
-  const mesFuturo = String(new Date().getMonth() + 2).padStart(2, '0')
+  let mesFuturo = String(new Date().getMonth() + 2).padStart(2, '0')
+
 
   const dataAtualFormatada = `${ano}-${mes}-01`
-  const dataFuturaFormatada = `${ano}-${mesFuturo}-01`
+  let dataFuturaFormatada = `${ano}-${mesFuturo}-01`
+  if (mesFuturo === '13') {
+    mesFuturo = '01'
+    const novoAno = ano + 1
+    dataFuturaFormatada = `${novoAno}-${mesFuturo}-01`
+  }
+
   const response = await axios.get(`http://100.29.19.154/api/relatorios/faturamento-empresa?dataInicio=${dataAtualFormatada}&dataFim=${dataFuturaFormatada}`, {
     headers: {
       Authorization: `Bearer ${token}`
@@ -15,6 +22,10 @@ export async function GetMonthlyGrossRevenue(token: string) {
   })
 
   const monthlyGrossRevenue: number = response.data
+
+  if (response.status === 400) {
+    return 0
+  }
 
   return monthlyGrossRevenue
 }
